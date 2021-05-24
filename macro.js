c52c3fa27f755eb6c7769704379f44382d055f72
Aaron Renner <arennerwv@gmail.com>
2021-05-24 19:16:03 -0400

    Updated macro, debug mode not functional

--- macro.js
+++ macro.js
@@ -1,14 +1,14 @@
 (function()
 {
     // Config options
+    var debug = true;
     var startRowNum = 2;
     var endRowNum = 10;
     
     // Varibale setup
     var oWorksheet = Api.GetActiveSheet();
     var baseUrl = "https://rest.coinapi.io/";
-    // Insert your coinapi.io key below
-    var apikey = "?apikey=KEY-HERE";
+    var apikey = "?apikey=<api-key-here>";
     var xmlhttp = new XMLHttpRequest();
     
     for (i = startRowNum; i < (endRowNum + 1); i++) {
@@ -18,13 +18,18 @@
         var endpoint = "v1/exchangerate/" + getCoinName + "/USD";
         var link = baseUrl + endpoint + apikey; // Build req URL
         
-        columnCValue.SetValue(""); // Clear old data
         xmlhttp.open("GET", link, false);
         xmlhttp.send();
-        
-        var coinVal = JSON.parse(xmlhttp.responseText).rate; // Parse JSON
-        columnCValue.SetValue(coinVal);
-        console.log(coinVal);
+        if(xmlhttp.status == 200) {
+            columnCValue.SetValue(""); // Clear old data
+            var coinVal = JSON.parse(xmlhttp.responseText).rate; // Parse JSON
+            columnCValue.SetValue(coinVal);
+        } else {
+            console.log(xmlhttp.status);
+        }
+        if(debug) {
+            console.log("Rate Limiting (used/total): " + xmlhttp.getResponseHeader("X-RateLimit-Used") + "/" + xmlhttp.getResponseHeader("X-RateLimit-Limit"));
+        }
     }
 }
 )();
